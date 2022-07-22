c87b44a3f601620188b533abd39f79da386fad11
wylee <dldndud627@gmail.com>
2022-07-22 11:43:44 +0900

    서버와 웹소켓 연결

--- src/view/chat/Chat.js
+++ src/view/chat/Chat.js
@@ -1,8 +1,48 @@
-function Chat() {
+import { Button } from 'reactstrap';
+import { useEffect } from 'react';
+
+const Chat = () => {
+    let websocket;
+
+    useEffect(() => {
+        console.log("Chat Component Mount");
+        wsConnect('ws://localhost:8080/ws/chat');
+    }, [])
+
+    const wsConnect = (url) => {
+        websocket = new WebSocket(url);
+
+        websocket.onopen = () => {
+            console.log("WebSocket opened...")
+        }
+
+        websocket.onmessage = (e) => {
+            console.log("Message : ", e)
+        }
+
+        websocket.onclose = () => {
+            console.log("WebSocket closed...");
+        }
+
+        websocket.onerror = (e) => {
+            console.log("WebSocket ERROR : ", e)
+        }
+
+    }
+
+    const clickButton = () => {
+        if (websocket !== null) {
+            websocket.send("send data");
+        }
+    }
+
 
     return (
         <div>
             CHAT
+            <Button onClick={clickButton}>
+                '버튼'
+            </Button>
         </div>
     )
 }
